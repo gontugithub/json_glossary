c70b2bb09f45e3c2028d3cab03df78ecb37a544a
gontugithub <gs.alvarez8@gmail.com>
2024-10-11 14:24:25 +0200

    estructura inicial

---
+++ src/main/java/com/example/json_glossary/JsonGlossaryApplication.java
@@ -0,0 +1,13 @@
+package com.example.json_glossary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonGlossaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonGlossaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/json_glossary/JsonGlossaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.json_glossary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonGlossaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
